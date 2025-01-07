b3e5fe0843ef47fba52ecdaec244529641677586
IsuruSH <shanakaisuru11@gmail.com>
2025-01-08 01:42:08 +0530

    feat: implement user authentication service with registration and login routes

---
+++ user_service/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import authRoutes from "./routes/authRoutes";
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+
+app.listen(4000, () => {
+  console.log(`Server running on port ${PORT}`);
+});
