dcc1f451e9d71f13986f5fda7391439db91c2b35
Thejana Hirumal <thejanahirumal5@gmail.com>
2025-02-18 11:22:31 +0530

    RCSIP-0033,0035,0036

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -22,6 +22,8 @@ app.use(
     credentials: true,
   })
 );
+
+
 app.use(express.json());
 
 // Auth routes (login, register)
@@ -48,6 +50,7 @@ const serviceOneProxy = createProxyMiddleware({
     }
   },
 });
+
 
 // const serviceTwoProxy = createProxyMiddleware({
 //   target: "http://service-two:8002",
